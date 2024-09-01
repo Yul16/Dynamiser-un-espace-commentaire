ca7de426bc449b61c10a9f4b6ff1e7fea3cd9be2
Yul <cmarcel01.pro@gmail.com>
2024-09-01 19:09:42 +0200

    add EventListener

---
+++ index.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.querySelector("form");
+  const commentList = document.getElementById("comment-list");
+  const errorMessage = document.getElementById("error-message");
+});
